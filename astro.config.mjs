4d24b56ba8601ae946669d427e452567aa9b33a4
Elliniafairy <elliniaharbor@outlook.com>
2025-07-01 01:06:15 +0900

    Initial configuration for fly.io.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	adapter: node({
+    mode: 'standalone',
+  }),
 });

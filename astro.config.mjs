b31f3ef48bf82a5f2a28157e686e3cfe72f2ff78
Elliniafairy <elliniaharbor@outlook.com>
2025-11-24 01:16:30 +0900

    Set site domain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://elliniafairy.space',
 	integrations: [mdx(), sitemap()],
 	adapter: node({
 		mode: 'standalone',
